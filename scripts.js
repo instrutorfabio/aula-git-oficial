44057747f9e30b1e84a2cd2e20269fd4795da3ed
Victor Trajano <trajanodev@hotmail.com>
2021-09-30 15:32:41 -0400

    task finalizada

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+const button = document.querySelector('button')
+
+button.addEventListener('click' , () => {
+
+    let inputContent = document.querySelector('input').value
+
+
+    const newLi = document.createElement('li')
+
+    newLi.textContent = inputContent
+
+    document.querySelector('ul').appendChild(newLi)
+    
+    console.log(inputContent)
+})
